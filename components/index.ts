a762498cd20d5c97e53e3ff721f26b11fb47bb36
azharkhan93 <azharkhan.work101@gmail.com>
2024-09-30 02:48:39 +0530

    new chnages

--- components/index.ts
+++ components/index.ts
@@ -1,13 +1,8 @@
 export * from "./HeroSection";
 export * from "./AboutSection";
-export * from "./VillaFeatures";
-export * from "./FooterSection";
-export * from "./FeaturedSection";
 export * from "./MultiBoxSection";
 export * from "./FullWidthBar";
 export * from "./CustomButton";
 export * from "./CustomLinkButton";
-export * from "./PointSection";
-export * from "./FormSection";
-export * from "./ArticleCard";
+
 

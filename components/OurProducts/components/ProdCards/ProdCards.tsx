085cf3188da18a9bd7583bb8441c38d7564c7a6b
azharkhan93 <azharkhan.work101@gmail.com>
2024-10-11 15:00:47 +0530

    animations removed

--- components/OurProducts/components/ProdCards/ProdCards.tsx
+++ components/OurProducts/components/ProdCards/ProdCards.tsx
@@ -8,16 +8,14 @@ type ProductCardProps = {
   buttonText: string;
 };
 
-export const ProdCards: React.FC<ProductCardProps> = ({ imageSrc, altText, buttonText }) => {
+export const ProdCards: React.FC<ProductCardProps> = ({
+  imageSrc,
+  altText,
+  buttonText,
+}) => {
   return (
     <div className="relative rounded-md md:w-[245px] w-[175px] shadow-sm shadow-black flex items-center justify-center">
-      <Image
-        src={imageSrc}
-        alt={altText}
-        width={240}
-        height={0}
-       
-      />
+      <Image src={imageSrc} alt={altText} width={240} height={100} />
       <div className="absolute bottom-[0px]  left-0 right-0 text-center bg-black/60 py-1 rounded cursor-pointer overflow-hidden w-full">
         <p className="text-white">
           <a href="https://mybillbook.in/store/pra_sweets" target="_blank">

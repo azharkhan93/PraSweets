c46194c02be57169222c390e1964f9825bd3e5d9
Sparrow Hawk <gzhb29orc@mozmail.com>
2024-10-14 17:05:01 +0530

    Logo Size Changed

--- components/HeroSection/HeroSection.tsx
+++ components/HeroSection/HeroSection.tsx
@@ -21,8 +21,8 @@ export const HeroSection: React.FC = () => {
           <Image
             src="/assets/images/logo3.png"
             alt="Logo"
-            width={180}
-            height={100}
+            width={280}
+            height={200}
           />
         </div>
         {/* Commentd for future release purpose */}
